34215d7a35238f0a5461959832d7235399f7328c
Nguyễn Văn Thái <91445147+thaikun2k2@users.noreply.github.com>
2022-06-24 23:20:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,19 @@
 const CONFIG = {
     titleWeb: "Mình tỏ tình với cậu đấy ạ",
     introTitle: 'Em là ai từ đâu bước đến nơi đây?',
-    introDesc: `Anh lạc vào trong mơ    
-    Nếu đây là giấc mơ thì hãy để anh chìm thật sâu
-    Anh không muốn thức dậy để lỡ vụt mất em nữa đâu
-    Nếu em có giống anh thì hãy đưa tay chạm vào nhau
-    Vì anh tin phép nhiệm màu sẽ biến giấc mơ thành sự thật
-    Can baby kiss me more
-    Kiss me more`,
+    introDesc: `… Hình như chính em,
+       (Cho anh mong chờ)
+    Hình như chính là em,
+       (Cho anh vấn vương)
+    Đừng thờ ơ, xin hãy lắng nghe
+       Và giúp anh trả lời đôi điều còn băn khoăn
+    … Có chắc yêu là đây, đây, đây
+       Có chắc yêu là đây, đây
+    Có chắc yêu là đây, đây, đây
+        Có chắc yêu là đây, đây
+    … Em lang thang cả ngày trong tâm trí
+        Đi không ngừng cả ngày trong tâm trí
+    Si mê thêm cuồng quay`,
     btnIntro: 'CHào Bạn Gái dễ Thương ghé qua trang Web này 🥰',
     title: 'Làm người yêu tớ nha 🥰',
     desc: '(Mình tỏ tình với cậu đấy cô gái đáng yêu ạ >-<)',
