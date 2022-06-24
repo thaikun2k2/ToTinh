fc9ed7e2e6fac53870626ad8379ac104ac228ef3
Nguyễn Văn Thái <91445147+thaikun2k2@users.noreply.github.com>
2022-06-24 23:06:54 +0700

    upload

--- config.js
+++ config.js
@@ -1,20 +1,23 @@
 const CONFIG = {
-    titleWeb: "Love confess",
-    introTitle: 'Who are you',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    titleWeb: "Mình tỏ tình với cậu đấy ạ",
+    introTitle: 'Em là ai từ đâu bước đến nơi đây?',
+    introDesc: `Anh lạc vào trong mơ    
+    Nếu đây là giấc mơ thì hãy để anh chìm thật sâu
+    Anh không muốn thức dậy để lỡ vụt mất em nữa đâu
+    Nếu em có giống anh thì hãy đưa tay chạm vào nhau
+    Vì anh tin phép nhiệm màu sẽ biến giấc mơ thành sự thật
+    Can baby kiss me more
+    Kiss me more`,
+    btnIntro: 'CHào Bạn Gái dễ Thương ghé qua trang Web này 🥰',
     title: 'Làm người yêu tớ nha 🥰',
-    desc: '',
+    desc: '(Mình tỏ tình với cậu đấy cô gái đáng yêu ạ >-<)',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :))',
-    question: 'Cho tớ biết lí do đi',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Em Đồng Ý Yêu Anh ',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Cho tớ biết lí do cậu thích minh đi',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Em Đồng Ý Yêu Anh Thái đẹp zai ',
+    mess: 'Mình biết mà 🥰. Yêu cậu nhiều lắm 😘😘',
+    messDesc: 'Nhắn tin qua messenger cho mình nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/thaikun2k2/'
 }
--- js/main.js
+++ js/main.js
@@ -32,10 +32,10 @@ function firstQuestion() {
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        imageUrl: 'img/logi.gif',
+        imageUrl: 'img/giphy.gif',
         imageWidth: 300,
         imageHeight: 300,
-        background: '#fff url("img/iput-bg.jpg")',
+        background: '#fff url("img/itMe.jpg")',
         imageAlt: 'Custom image',
         confirmButtonText: CONFIG.btnIntro
     }).then(function() {
@@ -114,7 +114,7 @@ $('#yes').click(function() {
         width: 900,
         padding: '3em',
         html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
-        background: '#fff url("img/iput-bg.jpg")',
+        background: '#fff url("img/itMe.jpg")',
         backdrop: `
               rgba(0,0,123,0.4)
               url("img/giphy2.gif")
@@ -129,7 +129,7 @@ $('#yes').click(function() {
             Swal.fire({
                 width: 900,
                 confirmButtonText: CONFIG.btnAccept,
-                background: '#fff url("img/iput-bg.jpg")',
+                background: '#fff url("img/iput-itMe.jpg")',
                 title: CONFIG.mess,
                 text: CONFIG.messDesc,
                 confirmButtonColor: '#83d0c9',
